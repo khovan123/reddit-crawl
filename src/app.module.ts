f638b953b675bbfc7dbe62d96703f907c7755768
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-06-28 15:09:47 +0700

    feat: use exact Reddit source URL resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { RedditCrawlerService } from './reddit/reddit-crawler.service';
 import { RedditCrawlerV2Service } from './reddit/reddit-crawler-v2.service';
 import { RedditOutputService } from './reddit/reddit-output.service';
 import { RedditSessionService } from './reddit/reddit-session.service';
+import { resolveRedditSourceUrl } from './reddit/reddit-source-url';
+import { RedditSourceConfig } from './reddit/reddit.types';
 
 @Module({
   controllers: [RedditController],
@@ -16,7 +18,24 @@ import { RedditSessionService } from './reddit/reddit-session.service';
       useFactory: (
         sessions: RedditSessionService,
         output: RedditOutputService,
-      ) => new RedditCrawlerV2Service(sessions, output),
+      ) => {
+        const crawler = new RedditCrawlerV2Service(sessions, output);
+        const runtimeCrawler = crawler as unknown as {
+          buildSourceUrl: (
+            source: RedditSourceConfig,
+            oldReddit: boolean,
+          ) => string;
+        };
+
+        Object.defineProperty(runtimeCrawler, 'buildSourceUrl', {
+          configurable: false,
+          enumerable: false,
+          writable: false,
+          value: resolveRedditSourceUrl,
+        });
+
+        return crawler;
+      },
     },
   ],
 })
