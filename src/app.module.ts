3342f2133ac183b3349d4aa28e3009ab0f38c2bc
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-06-28 16:30:35 +0700

    switch crawler to robust feed enhancements

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { RedditController } from './reddit/reddit.controller';
-import { applyCrawlerRecovery } from './reddit/reddit-crawler-recovery';
+import { applyCrawlerEnhancements } from './reddit/reddit-crawler-enhancements';
 import { RedditCrawlerService } from './reddit/reddit-crawler.service';
 import { RedditCrawlerV2Service } from './reddit/reddit-crawler-v2.service';
 import { RedditOutputService } from './reddit/reddit-output.service';
@@ -21,7 +21,7 @@ import { RedditSourceConfig } from './reddit/reddit.types';
         output: RedditOutputService,
       ) => {
         const crawler = new RedditCrawlerV2Service(sessions, output);
-        applyCrawlerRecovery(crawler);
+        applyCrawlerEnhancements(crawler);
 
         const runtimeCrawler = crawler as unknown as {
           buildSourceUrl: (
